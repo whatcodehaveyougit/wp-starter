5ce1195e8d183a1e9aa7306766fe41e4f6e547e4
whatcodehaveyougit <sigurdwatt@gmail.com>
2024-12-04 19:34:50 +0000

    Add auto version bump to functions.php;

--- themes/wp-child-theme-template/functions.php
+++ themes/wp-child-theme-template/functions.php
@@ -1,12 +1,17 @@
 <?php
 
-
 add_action( 'wp_enqueue_scripts', 'twentytwentyfour_child_scripts' );
 function twentytwentyfour_child_scripts() {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
+    // Get the file modification time for cache busting
+    $child_style_version = filemtime( get_stylesheet_directory() . '/style.css' );
+    $parcel_css_version = filemtime( get_stylesheet_directory() . '/dist/styles/style.css' );
+    $parcel_js_version = filemtime( get_stylesheet_directory() . '/dist/scripts/scripts.js' );
 
+    // Enqueue styles
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ), $child_style_version );
+    wp_enqueue_style( 'parcel', get_stylesheet_directory_uri() . '/dist/styles/style.css', array(), $parcel_css_version );
 
-	wp_enqueue_style( 'parcel', get_stylesheet_directory_uri() . '/dist/styles/style.css', array(), '1.0' );
-	wp_enqueue_script( 'parcel-js', get_stylesheet_directory_uri() . '/dist/scripts/scripts.js', array(), '1.0', true );
+    // Enqueue scripts
+    wp_enqueue_script( 'parcel-js', get_stylesheet_directory_uri() . '/dist/scripts/scripts.js', array(), $parcel_js_version, true );
 }
